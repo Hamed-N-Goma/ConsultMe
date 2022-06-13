8daf72b8b892e335b5dc2b9d02bdc91e097d3252
Hamed Goma <78850630+Hamed-N-Goma@users.noreply.github.com>
2022-06-13 14:31:20 +0200

    edit profile photo

--- lib/presentation_layer/user/widget/toprated.dart
+++ lib/presentation_layer/user/widget/toprated.dart
@@ -105,7 +105,7 @@ class Toprated extends StatelessWidget {
                   navigateTo(
                       context,
                       consultantDetails(
-                        cm: consultant,
+                        consultant: consultant,
                       ));
                 },
                 child: FaIcon(
