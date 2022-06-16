532776845b66df6c5718d93f3dee9e83ad8807b5
jw3ly <45406408+jw3ly@users.noreply.github.com>
2022-06-16 18:48:05 +0200

    chat

--- lib/presentation_layer/consultant/chat.dart
+++ lib/presentation_layer/consultant/chat.dart
@@ -2,9 +2,11 @@ import 'package:conditional_builder_null_safety/conditional_builder_null_safety.
 import 'package:consultme/Bloc/consultantBloc/cubit/consultant_cubit.dart';
 import 'package:consultme/Bloc/consultantBloc/cubit/consultant_states.dart';
 import 'package:consultme/presentation_layer/consultant/messender_main.dart';
+import 'package:consultme/presentation_layer/presentation_layer_manager/color_manager/color_manager.dart';
 import 'package:consultme/presentation_layer/user/widget/messenger_main.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 
 class ConsultChat extends StatelessWidget {
@@ -23,25 +25,34 @@ class ConsultChat extends StatelessWidget {
           width = size.width;
           return Directionality(
             textDirection: TextDirection.rtl,
-            child: ConditionalBuilder(
-              condition: ConsultantCubit
-                  .get(context)
-                  .usersInChat
-                  .length > 0,
-              builder: (context) =>
-                  ListView.separated(
-                      physics: BouncingScrollPhysics(),
-                      itemBuilder: (context, index) =>
-                          ConsultChatitem(ConsultantCubit
-                              .get(context)
-                              .usersInChat[index], context),
-                      separatorBuilder: (context, index) => myDivider(),
-                      itemCount: ConsultantCubit
-                          .get(context)
-                          .usersInChat
-                          .length),
-              fallback: (context) => Center(child: CircularProgressIndicator()),
+            child: Scaffold(
+              appBar: AppBar(
+                backgroundColor: Theme.of(context).appBarTheme.backgroundColor,
+                elevation: Theme.of(context).appBarTheme.elevation,
+                foregroundColor: Theme.of(context).appBarTheme.foregroundColor,
+                title: leadingTitle(),
+               // actions: appBarItems(model?.image),
+              ),
+              body: ConditionalBuilder(
+                condition: ConsultantCubit
+                    .get(context)
+                    .usersInChat
+                    .length > 0,
+                builder: (context) =>
+                    ListView.separated(
+                        physics: BouncingScrollPhysics(),
+                        itemBuilder: (context, index) =>
+                            ConsultChatitem(ConsultantCubit
+                                .get(context)
+                                .usersInChat[index], context),
+                        separatorBuilder: (context, index) => myDivider(),
+                        itemCount: ConsultantCubit
+                            .get(context)
+                            .usersInChat
+                            .length),
+                fallback: (context) => Center(child: CircularProgressIndicator()),
 
+              ),
             ),
           );
         },
@@ -59,3 +70,35 @@ Widget myDivider() => Padding(
     color: Colors.grey[300],
   ),
 );
+Widget leadingTitle() {
+  return Text(
+    'إستشرني',
+  //  style: Theme.of(context).textTheme.bodyText1,
+  );
+}
+
+List<Widget> appBarItems(image) {
+  return [
+    IconButton(
+      onPressed: () {},
+      icon: const FaIcon(
+        FontAwesomeIcons.bell,
+        color: ColorManager.myGrey,
+      ),
+    ),
+    InkWell(
+      onTap: () {
+      //  navigateTo(context, const Profile());
+      },
+      child: CircleAvatar(
+        backgroundImage: image == null
+            ? const AssetImage(
+          "assets/images/user.png",
+        ) as ImageProvider
+            : NetworkImage(image),
+        radius: 15,
+      ),
+    )
+  ];
+}
+
